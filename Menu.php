24e3156b5d27e9a07e5b41440e78433467bbe2b9
JulieAth <47970693+JulieAth@users.noreply.github.com>
2019-03-17 22:51:46 +0100

    Create Menu.php

---
+++ Menu.php
@@ -0,0 +1,14 @@
+<h1> Univoit’ </h1>
+<p>
+    <img src="logosite.png" alt="logo du site" />
+</p>
+
+<nav>
+    <ul>
+        <li> <a href="accueil.php">Accueil</a> </li>
+        <li> <a href="rechercher.php">Rechercher un trajet</a> </li>
+        <li> <a href="proposer.php">Proposer un trajet</a> </li>
+        <li> <a href="profil.php">Profil</a> </li>
+    </ul>
+</nav>
+
